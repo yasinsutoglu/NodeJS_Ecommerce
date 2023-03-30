bb5683d7a35f5b867bf84840c5f7ca4fdd8b2e80
Yasin Sutoglu <ysutoglu@gmail.com>
2023-03-30 13:13:08 +0300

    auth issues structure

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,10 @@ const morgan = require('morgan')
 //database
 const connectDB = require('./db/connect')
 
+
+//routers
+const authRouter = require('./routes/authRoutes')
+
 //middleware
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require("./middleware/error-handler");
@@ -21,6 +25,8 @@ app.use(express.json())
 app.get('/', (req,res)=>{
     res.send('e-commerce api')
 })
+
+app.use('/api/v1/auth', authRouter);
 
 // /apples
 
