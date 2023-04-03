60556d4fe360e818238c7447f63c89e16bbb963c
Yasin Sutoglu <ysutoglu@gmail.com>
2023-04-03 11:08:21 +0300

    updates

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,26 @@
-const User = require('../models/User');
+const User = require("../models/User");
+const {StatusCodes} = require('http-status-codes');
+const CustomError = require('../errors')
+const jwt = require('jsonwebtoken')
 
 const register = async(req,res)=>{
-    res.send('register user');
+    const {email , name , password} = req.body;
+
+    const emailAlreadyExists = await User.findOne({email})
+    if(emailAlreadyExists){
+        throw new CustomError.BadRequestError('Email already exists')
+    }
+
+    //first registered user is an admin
+    const isFirstAccount = await User.countDocuments({}) === 0;
+    const role = isFirstAccount ? 'admin' : 'user';
+
+    const user = await User.create({name,email,password,role})
+
+    const tokenUser = {name:user.name, userId:user._id, role:user.role}
+    const token = jwt.sign(tokenUser,process.env.JWT_SECRET,{expiresIn: process.env.JWT_LIFETIME})
+
+    res.status(StatusCodes.CREATED).json({user:tokenUser, token});
 }
 
 const login = async (req, res) => {
