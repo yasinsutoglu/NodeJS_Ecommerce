18c159499eb3c8a2847faf9a26e31f358ba36e6d
Yasin Sutoglu <ysutoglu@gmail.com>
2023-04-05 12:32:16 +0300

    aggregation pipeline issue

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,67 +1,74 @@
 const mongoose = require('mongoose');
 
-const ProductSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: [true, "Please provide product name"],
-    maxlength: [100, "Name can not be more than 100 chars"],
-  },
-  price: {
-    type: Number,
-    required: [true, "Please provide product price"],
-    default: 0,
-  },
-  description: {
-    type: String,
-    required: [true, "Please provide product description"],
-    maxlength: [1000, "Description can not be more than 1000 chars"],
-  },
-  image: {
-    type: String,
-    default: "/uploads/example.jpeg",
-  },
-  category: {
-    type: String,
-    required: [true, "Please provide product category"],
-    enum: ["office", "kitchen", "bedroom"],
-  },
-  company: {
-    type: String,
-    required: [true, "Please provide product company"],
-    enum: {
-      values: ["ikea", "liddy", "marcos"],
-      message: "{VALUE} is not supported",
+const ProductSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "Please provide product name"],
+      maxlength: [100, "Name can not be more than 100 chars"],
+    },
+    price: {
+      type: Number,
+      required: [true, "Please provide product price"],
+      default: 0,
+    },
+    description: {
+      type: String,
+      required: [true, "Please provide product description"],
+      maxlength: [1000, "Description can not be more than 1000 chars"],
+    },
+    image: {
+      type: String,
+      default: "/uploads/example.jpeg",
+    },
+    category: {
+      type: String,
+      required: [true, "Please provide product category"],
+      enum: ["office", "kitchen", "bedroom"],
+    },
+    company: {
+      type: String,
+      required: [true, "Please provide product company"],
+      enum: {
+        values: ["ikea", "liddy", "marcos"],
+        message: "{VALUE} is not supported",
+      },
+    },
+    colors: {
+      type: [String],
+      default: ["#222"],
+      required: true,
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+    freeShipping: {
+      type: Boolean,
+      default: false,
+    },
+    inventory: {
+      type: Number,
+      required: true,
+      default: 15,
+    },
+    averageRating: {
+      type: Number,
+      default: 0,
+    },
+    numOfReviews: {
+      type: Number,
+      default: 0,
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
   },
-  colors: {
-    type: [String],
-    default:['#222'],
-    required: true,
-  },
-  featured: {
-    type: Boolean,
-    default: false,
-  },
-  freeShipping: {
-    type: Boolean,
-    default:false,
-  },
-  inventory:{
-    type:Number,
-    required:true,
-    default:15,
-  },
-  averageRating:{
-    type:Number,
-    default:0,
-  },
-  user:{
-    type:mongoose.Types.ObjectId,
-    ref:'User',
-    required:true,
-  },
-}, {timestamps:true, toJSON:{virtuals:true}, toObject:{virtuals:true}});
+  { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
+);
 
 ProductSchema.virtual('reviews', {
   ref:'Review',
@@ -74,5 +81,7 @@ ProductSchema.virtual('reviews', {
 ProductSchema.pre('remove', async function(next){
   await this.model('Review').deleteMany({product:this._id});
 })
+
+
 
 module.exports = mongoose.model('Product', ProductSchema)
